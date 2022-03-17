11f46df2294fbc52bb27b4e8aa2f76758a0d7720
Simon Bochkarev <kemer0v0@mail.ru>
2022-03-17 23:14:55 +0700

    Добавьте файлы проекта.

---
+++ RegexApp/Models/RegexA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegexApp.Models
+{
+    public class RegexA
+    {
+
+        public static string? FindRegexInText(string text, string pattern)
+        {
+            string result = "";
+            Regex rex = new Regex(pattern);
+            MatchCollection y = rex.Matches(text);
+            foreach (Match x in y)
+            {
+                result += (x.Value + "\n");
+            }
+            return result;
+        }
+    }
+}
